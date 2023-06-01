75fd3a62e1caf81869f19a9acc563c505c9f7b63
SuRakib <rakibul15-10976@diu.edu.bd>
2023-06-01 07:07:37 +0600

    Fourth Commit(Dialog add with binding)

--- app/src/main/java/com/example/tictactoegame/GameActivity.kt
+++ app/src/main/java/com/example/tictactoegame/GameActivity.kt
@@ -1,16 +1,25 @@
 package com.example.tictactoegame
 
+import android.app.Dialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.Window
 import android.widget.Button
 import android.widget.Toast
+import com.example.tictactoegame.databinding.ActivityGameBinding
+import com.example.tictactoegame.databinding.ResultAlertBinding
 
 class GameActivity : AppCompatActivity() {
+
+    private  lateinit var binding: ActivityGameBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game)
+        binding=ActivityGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
         fun buClick(view: View){
@@ -118,35 +127,35 @@ class GameActivity : AppCompatActivity() {
             }
 
             //col1 winner
-            if(player1.contains(1) && player1.contains(4) &&player1.contains(7)){
+            if(player1.contains(7) && player1.contains(1) &&player1.contains(4)){
 
                 winner=1
 
             }
-            if(player2.contains(1) && player2.contains(4) &&player2.contains(7)){
+            if(player2.contains(7) && player2.contains(1) &&player2.contains(4)){
 
                 winner=2
 
             }
             //col2 winner
-            if(player1.contains(2) && player1.contains(5) &&player1.contains(8)){
+            if(player1.contains(8) && player1.contains(2) &&player1.contains(5)){
 
                 winner=1
 
             }
-            if(player2.contains(2) && player2.contains(5) &&player2.contains(8)){
+            if(player2.contains(8) && player2.contains(2) &&player2.contains(5)){
 
                 winner=2
 
             }
             //col3 winner
 
-            if(player1.contains(3) && player1.contains(6) &&player1.contains(9)){
+            if(player1.contains(9) && player1.contains(3) &&player1.contains(6)){
 
                 winner=1
 
             }
-            if(player2.contains(3) && player2.contains(6) &&player2.contains(9)){
+            if(player2.contains(9) && player2.contains(3) &&player2.contains(6)){
 
                 winner=2
 
@@ -154,12 +163,12 @@ class GameActivity : AppCompatActivity() {
 
             //cross 1 winner
 
-            if(player1.contains(1) && player1.contains(5) &&player1.contains(9)){
+            if(player1.contains(4) && player1.contains(2) &&player1.contains(9)){
 
                 winner=1
 
             }
-            if(player2.contains(1) && player2.contains(5) &&player2.contains(9)){
+            if(player2.contains(4) && player2.contains(2) &&player2.contains(9)){
 
                 winner=2
 
@@ -167,12 +176,12 @@ class GameActivity : AppCompatActivity() {
 
             //cross 2 winner
 
-            if(player1.contains(3) && player1.contains(5) &&player1.contains(7)){
+            if(player1.contains(7) && player1.contains(2) &&player1.contains(6)){
 
                 winner=1
 
             }
-            if(player2.contains(3) && player2.contains(5) &&player2.contains(7)){
+            if(player2.contains(7) && player2.contains(2) &&player2.contains(6)){
 
                 winner=2
 
@@ -182,54 +191,73 @@ class GameActivity : AppCompatActivity() {
 
             if(winner==1){
 
-                Toast.makeText(this," Player 1 win this match", Toast.LENGTH_LONG).show()
-
-
-//
-//                val dialog=Dialog(this)
-//        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-//        dialog.setCancelable(false)
-//            dialog.setContentView(R.layout.result_alert)
-//            dialog.result.text = "Player 1 win the Game"
-//            dialog.plyBtn.setOnClickListener{
-//
-//                val intent = Intent(this,MainActivity::class.java)
-//                finish()
-//                startActivity(intent)
-//
-//            }
-//            dialog.exitBtn.setOnClickListener{
-//                val intent= Intent(this,GameActivity::class.java)
-//                startActivity(intent)
-//            }
-//            dialog.show()
+               // Toast.makeText(this," Player 1 win this match", Toast.LENGTH_LONG).show()
 
 
 
-            }else if(winner==2){
+                val dialog= Dialog(this)
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+                dialog.setCancelable(false)
+                val binding = ResultAlertBinding.inflate(LayoutInflater.from(this))
+                val resultTextView = binding.result
 
-                Toast.makeText(this,"Player 2 win this match", Toast.LENGTH_LONG).show()
+// Set the text of the `result` TextView
+                resultTextView.text = "Player 1 win the Game"
+
+// Set the content view of the dialog to the inflated layout
+                dialog.setContentView(binding.root)
+
+                val playButton=binding.plyBtn
+                playButton.setOnClickListener{
+                    val intent= Intent(this,GameActivity::class.java)
+
+                    finish()
+                    startActivity(intent)
+
+                }
+                val exitButton=binding.exitBtn
+                exitButton.setOnClickListener{
+                    val intent = Intent(this,MainActivity::class.java)
+                    startActivity(intent)
+                }
+                dialog.show()
+
+
+            }
+
+            else if(winner==2){
+
+                //  Toast.makeText(this,"Player 2 win this match",Toast.LENGTH_LONG).show()
 
 
 
-//
-//            val dialog=Dialog(this)
-//            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-//            dialog.setCancelable(false)
-//            dialog.setContentView(R.layout.result_alert)
-//            dialog.result.text = "Player 2 win the Game"
-//            dialog.plyBtn.setOnClickListener{
-//
-//                val intent = Intent(this,MainActivity::class.java)
-//                finish()
-//                startActivity(intent)
-//
-//            }
-//            dialog.exitBtn.setOnClickListener{
-//                val intent= Intent(this,GameActivity::class.java)
-//                startActivity(intent)
-//            }
-//            dialog.show()
+
+                val dialog=Dialog(this)
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+                dialog.setCancelable(false)
+                val binding = ResultAlertBinding.inflate(LayoutInflater.from(this))
+                val resultTextView = binding.result
+
+// Set the text of the `result` TextView
+                resultTextView.text = "Player 2 win the Game"
+
+// Set the content view of the dialog to the inflated layout
+                dialog.setContentView(binding.root)
+
+                val playButton=binding.plyBtn
+                playButton.setOnClickListener{
+                    val intent= Intent(this,GameActivity::class.java)
+
+                    finish()
+                    startActivity(intent)
+
+                }
+                val exitButton=binding.exitBtn
+                exitButton.setOnClickListener{
+                    val intent = Intent(this,MainActivity::class.java)
+                    startActivity(intent)
+                }
+                dialog.show()
 
 
             }
